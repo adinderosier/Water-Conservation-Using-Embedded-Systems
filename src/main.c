8b19e0d686de9e975fbc332344977d4f04ae463a
adinderosier <adinderosier13@gmail.com>
2022-11-08 01:27:55 -0800

    feat(Main): Added wireless connectivity and testing uart interrupt requests

--- src/main.c
+++ src/main.c
@@ -6,7 +6,8 @@
 #include "pico/cyw43_arch.h"
 #include "hardware/gpio.h"
 #include "hardware/uart.h"
-
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
 
 #define UART_ID uart0
 #define BAUD_RATE PICO_DEFAULT_UART_BAUD_RATE
@@ -26,6 +27,48 @@ void heartbeat_task( void *pvParameters )
 }
 
 
+void uart_rx_isr()
+{
+    while ( uart_is_readable( UART_ID ) )
+    {
+        uint8_t ch = uart_getc( UART_ID );
+
+        if ( uart_is_writable( UART_ID ) )
+        {
+            uart_putc( UART_ID, ch );
+            printf( "Received UART data VIA ISR: %c\n", ch );
+        }
+    }
+}
+
+
+int sta_init( void *pvParameters )
+{
+    if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA))
+    {
+        printf("CYW43 ARCH: failed to initialise\n");
+        return 1;
+    }
+
+    cyw43_arch_enable_sta_mode();
+
+    cyw43_wifi_pm(&cyw43_state, 0xa11140);
+
+    printf("Connecting to WiFi...\n");
+    printf("SSID: %s\n", WIFI_SSID);
+    printf("Password: %s\n", WIFI_PASSWORD);
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000))
+    {
+        printf("Wireless connection failed.\n");
+        return 1;
+    }
+    else
+    {
+        printf("Wireless connected.\n");
+    }
+}
+
+
 int main()
 {
     // Setup the USB as as a serial port
@@ -39,14 +82,25 @@ int main()
     gpio_set_function( UART_TX_PIN, GPIO_FUNC_UART );
     gpio_set_function( UART_RX_PIN, GPIO_FUNC_UART );
 
+    // Set up a RX interrupt
+    // We need to set up the handler first
+    // Select correct interrupt for the UART we are using
+    int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
+
+    // And set up and enable the interrupt handlers
+    irq_set_exclusive_handler(UART_IRQ, uart_rx_isr);
+    irq_set_enabled(UART_IRQ, true);
+
+    // Now enable the UART to send interrupts - RX only
+    uart_set_irq_enables(UART_ID, true, false);
+
     // Delay to allow for the USB serial to be ready
     sleep_ms( 5000 );
 
-    if ( cyw43_arch_init_with_country( CYW43_COUNTRY_USA ) ){ printf("WiFi init failed"); }
+    printf( "Initialising WiFi...\n" );
+    sta_init( NULL );
 
-    printf( "Starting FreeRTOS...\n" );
-    sleep_ms( 1000 );
-
+    printf("Starting FreeRTOS...\n");
     printf( "Starting Heartbeat Task...\n" );
     xTaskCreate( heartbeat_task, "Heartbeat Task", 256, NULL, 1, NULL );
 
